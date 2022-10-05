6584710ef0ba746cfb59f5437d1cbc4cb0cd6920
JoseAngelEsquivelIslas <115033210+JoseAngelEsquivelIslas@users.noreply.github.com>
2022-10-05 10:52:43 -0500

    Dev (#4)
    * Se agrega estructura inicial
    
    * Se agrega .gitignore
    
    * Se actualiza versión de java en config.yml de circleci
    
    * Se agrega configuración de datasource y se actualiza el pom.xml
    
    * Actualiza la versión de la imagen de circle CI de Java 11 a Java 17
    
    * Agrega valores por default a la configuración de la base de datos y la configuración inicial de flyway.
    
    * Arregla code smells reportados por Sonar
    
    * Trigger Build
    
    Co-authored-by: Jose Esquivel <jose@server-jose.debian.local>
    Co-authored-by: kumo829 <amonmar2000@hotmail.com>

---
+++ src/main/java/com/mozcalti/gamingapp/GamingAppApplication.java
@@ -0,0 +1,14 @@
+package com.mozcalti.gamingapp;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamingAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mozcalti/gamingapp/GamingAppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mozcalti.gamingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class GamingAppApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
