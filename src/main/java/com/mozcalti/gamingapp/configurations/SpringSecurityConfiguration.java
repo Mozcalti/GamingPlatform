210bcf9aeff96523788e1547c718c010d457f351
Jose <joseangelesquivelislas@gmail.com>
2022-10-07 12:09:05 -0500

    se agrega configuración de spring security (actualizado)

--- src/main/java/com/mozcalti/gamingapp/configurations/SpringSecurityConfiguration.java
+++ src/main/java/com/mozcalti/gamingapp/configurations/SpringSecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +11,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/institucion/**").permitAll()
-                .anyRequest().authenticated();
+        http.cors().and().csrf().disable();
+        http.csrf().ignoringAntMatchers("/institucion/**");
 
         return http.build();
     }
