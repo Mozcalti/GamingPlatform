e7089c3d97c7a471e4ff6cdc4c3894d4dbdabccf
kumo829 <amonmar2000@hotmail.com>
2022-10-15 11:55:34 -0700

    Agrega configuración de Spring Security para agregar manejadores de eventos y rutas

--- src/main/java/com/mozcalti/gamingapp/configurations/SpringSecurityConfiguration.java
+++ src/main/java/com/mozcalti/gamingapp/configurations/SpringSecurityConfiguration.java
@@ -1,19 +1,53 @@
 package com.mozcalti.gamingapp.configurations;
 
+import com.mozcalti.gamingapp.secutiry.AccessDeniedHandlerImpl;
+import com.mozcalti.gamingapp.secutiry.AuthencationFailureHandlerImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SpringSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().ignoringAntMatchers("/institucion/**");
+        http
+                .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                    .authorizeRequests()
+                    .mvcMatchers("/api/login", "/api/logout").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginProcessingUrl("/api/login")
+                    .failureUrl("/login_error")
+                    .failureHandler(new AuthencationFailureHandlerImpl())
+                    .defaultSuccessUrl("/miPerfil")
+                .and()
+                    .logout().logoutUrl("/api/logout")
+                    .logoutSuccessUrl("/")
+                .and()
+                    .exceptionHandling()
+                    .accessDeniedPage("/access_denied")
+                    .accessDeniedHandler(new AccessDeniedHandlerImpl());
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web
+                .ignoring()
+                .antMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
     }
 
 }
