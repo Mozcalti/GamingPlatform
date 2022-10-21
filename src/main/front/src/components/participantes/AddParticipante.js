378d1272c80442e84f817d9529ec3c726af2af4f
Jose <joseangelesquivelislas@gmail.com>
2022-10-20 22:20:28 -0500

    Se quitan dependencias no utilizadas

--- src/main/front/src/components/participantes/AddParticipante.js
+++ src/main/front/src/components/participantes/AddParticipante.js
@@ -1,5 +1,5 @@
 import DialogTitle from "@mui/material/DialogTitle";
-import {Divider, Grid, Stack, TextField} from "@mui/material";
+import {Divider, Stack, TextField} from "@mui/material";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
