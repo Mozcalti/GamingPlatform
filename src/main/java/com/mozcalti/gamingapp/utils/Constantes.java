02babb8bc86d26737018975e32a151cac1d57de0
jruiztahuilan <114958205+jruiztahuilan@users.noreply.github.com>
2022-10-30 23:32:57 -0600

    Ajuste al scheduler que carga xml de resultados debido a cambios en la base de datos

--- src/main/java/com/mozcalti/gamingapp/utils/Constantes.java
+++ src/main/java/com/mozcalti/gamingapp/utils/Constantes.java
@@ -26,7 +26,9 @@ public final class Constantes {
     public static final String TODOS = "todos";
     public static final String SEPARA_NOM_PARTICIPANTES = ", ";
     public static final String SEPARA_MAILS = ",";
-
     public static final String ESPACIO = " ";
+    public static final String DIAGONAL = "/";
+
+    public static final String XML = ".xml";
 
 }
