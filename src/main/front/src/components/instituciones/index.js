8701c8c6ca3ab1f66606612be00e99278007aeb9
mikeycabrera <50428939+mikeycabrera@users.noreply.github.com>
2022-11-01 21:57:29 -0600

    Carga masiva Instituciones (#47)
    * Carga masiva Instituciones
    
    * Carga masiva Instituciones (Se corrige error de sonar)
    
    Co-authored-by: miguel.cabrera.munoz.contractor <miguel.cabrera.munoz.contractor@bbva.com>

--- src/main/front/src/components/instituciones/index.js
+++ src/main/front/src/components/instituciones/index.js
@@ -1,34 +1,45 @@
 import ResponsiveAppBar from "../appBar/AppBar";
 import React, {useEffect, useState} from "react";
-import {Avatar, Container, Divider, Grid, TextField, Typography} from "@mui/material";
 import Button from "@mui/material/Button";
 import {DataGrid} from "@mui/x-data-grid";
 import InstitucionesService from "./Instituciones.service";
 import DetalleInstitucion from "./DetalleInstitucion";
 import * as Yup from "yup";
-import moment from "moment/moment";
-
-import {AdapterMoment} from '@mui/x-date-pickers/AdapterMoment';
-import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
-import {DatePicker} from "@mui/x-date-pickers/DatePicker";
-
+import {
+    Alert,
+    Container,
+    Divider,
+    Grid, Snackbar,
+    TextField,
+    Typography
+} from "@mui/material";
+import { CargaMasivaInstituciones } from "./CargaMasivaInstituciones";
 const InstitucionesList = () => {
     const [instituciones, setInstituciones] = useState([]);
     const [message, setMessage] = useState("");
-    const [fecha, setFecha] = useState(null);
-
-
-    const getInstituciones = (texto, fechaCreacion, indice) => {
-        if (fechaCreacion === '' || fechaCreacion === null)
-            fechaCreacion = '';
-        else
-            fechaCreacion = moment(fechaCreacion).format("yyyy-MM-DD HH:mm:ss.SSSSSS")
-
-        InstitucionesService.lista(texto, fechaCreacion, indice)
+    const [resultado, setResultado] = useState({
+        sucess: false,
+        error: false
+    })
+    const [errorResponse, setErrorResponse] = useState("");
+    const getInstituciones = (texto, indice) => {
+        InstitucionesService.lista(texto,indice)
             .then(
                 (response) => {
                     setInstituciones(response.data.lista);
-                    setFecha(null)
+                },
+                error => {
+                    console.log(error)
+                }
+            );
+    }
+
+    const getInstitucionMasiva = (texto, indice) => {
+        InstitucionesService.getInstitucion(texto,indice)
+            .then(
+                (response) => {
+                    console.log(response.data);
+                    setInstituciones(response.data);
                 },
                 error => {
                     console.log(error)
@@ -38,8 +49,29 @@ const InstitucionesList = () => {
 
 
     useEffect(() => {
-        getInstituciones("", "", 0)
+        getInstitucionMasiva();
     }, []);
+    
+    const validaExcel =(data)=>{
+        InstitucionesService.validaExcelInstitucion(data)
+        .then((response)=>{
+            setResultado({...resultado,sucess:true})
+            setInstituciones(response.data);
+        },
+        error=>{
+            setErrorResponse(error.response.data.message);
+            setResultado({...resultado, error: true});
+        }
+        )
+    }
+
+    const ValidaFormCargaMasiva = Yup.object().shape({
+        file: Yup.mixed()
+            .test("file", "El archivo es obligatorio", (value) => {if (value.length > 0)return true;else return false;})
+            .test("fileType", "El formato del archivo no es valido", (value) =>{
+                return value.length && ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.ms-excel"].includes(value[0].type)
+            })
+    });
 
 
     const ValidaForm = Yup.object().shape({
@@ -54,26 +86,11 @@ const InstitucionesList = () => {
 
 
     const columns = [
+        {field: 'nombre', headerName: 'Nombre', filterable: false ,width: 300},
+        {field: 'correo', headerName: 'Correo Electronico', filterable: false , width: 300},
+        {field: 'fechaCreacion', headerName: 'Fecha de Creación', filterable: false , width: 300},
         {
-            field: 'logo', headerName: 'Logo', filterable: false, width: 60, renderCell: (params) => {
-                return (
-                    <Avatar src={params.value} alt="Remy Sharp" sx={{width: 40, height: 40,}}/>
-                );
-            }
-        },
-        {field: 'nombre', headerName: 'Nombre', filterable: false, width: 300},
-        {field: 'correo', headerName: 'Correo Electronico', filterable: false, width: 300},
-        {
-            field: 'fechaCreacion',
-            headerName: 'Fecha de Creación',
-            filterable: false,
-            width: 300,
-            renderCell: (params) => {
-                return moment(params.value).format("DD/MM/yyyy HH:mm:SS a");
-            }
-        },
-        {
-            field: 'id', headerName: 'Acciones', filterable: false, width: 150, renderCell: (p) => {
+            field: 'id', headerName: 'Acciones', filterable: false , width: 225, renderCell: (p) => {
                 return (
                     <Grid container spacing={2}>
                         <Grid item xs={6}>
@@ -81,8 +98,10 @@ const InstitucionesList = () => {
                         </Grid>
                     </Grid>
                 )
+
             }
         }];
+
 
     return (
         <>
@@ -95,26 +114,17 @@ const InstitucionesList = () => {
                 <div style={{height: 400}}>
                     <Grid container spacing={2}>
                         <Grid item xs={6} md={6}>
-                            <TextField label="Busqueda" variant="outlined" fullWidth
-                                       onChange={(e) => setMessage(e.target.value)}/>
-                        </Grid>
-
-                        <Grid item xs={4} md={4}>
-                            <LocalizationProvider dateAdapter={AdapterMoment}>
-                                <DatePicker
-                                    renderInput={(props) => <TextField {...props} />}
-                                    inputFormat="DD/MM/YYYY"
-                                    label="Fecha de creación"
-                                    value={fecha}
-                                    onChange={(e) => {
-                                        setFecha(e)
-                                    }}
-                                />
-                            </LocalizationProvider>
+                            <TextField label="Busqueda" variant="outlined" fullWidth  onChange={(e) => setMessage(e.target.value)}/>
                         </Grid>
                         <Grid item xs={2} md={2}>
-                            <Button onClick={() => getInstituciones(message, fecha, 0)} variant="contained"
-                                    size="large">Buscar</Button>
+                            <Button onClick={() => getInstituciones(message,0)} variant="contained" size="large">Buscar</Button>
+                        </Grid>
+                        <Grid item xs={2} md={2}>
+                            <CargaMasivaInstituciones 
+                            validaExcel={validaExcel}
+                            ValidaFormCargaMasiva={ValidaFormCargaMasiva}></CargaMasivaInstituciones>
+                        </Grid>
+                        <Grid item xs={2} md={2}>
                         </Grid>
                     </Grid>
                     <br/>
@@ -129,6 +139,16 @@ const InstitucionesList = () => {
                     />
                 </div>
             </Container>
+            <Snackbar open={resultado.success} autoHideDuration={6000} onClose={() => {
+                setResultado({...resultado, success: false})
+            }}>
+                <Alert severity="success" variant="filled">Se guardo correctamente las instituciones</Alert>
+            </Snackbar>
+            <Snackbar open={resultado.error} autoHideDuration={6000} onClose={() => {
+                setResultado({...resultado, error: false})
+            }}>
+                <Alert severity="error" variant="filled">{errorResponse}</Alert>
+            </Snackbar>
         </>
     );
 };
