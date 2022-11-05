f8d404c350ed8cfa303cacd90688585fe1eaef23
jruiztahuilan <114958205+jruiztahuilan@users.noreply.github.com>
2022-11-05 12:25:15 -0600

    Se corrige errores del sonar

--- src/main/java/com/mozcalti/gamingapp/service/correos/SendMailInvitacionSevice.java
+++ src/main/java/com/mozcalti/gamingapp/service/correos/SendMailInvitacionSevice.java
@@ -1,7 +1,5 @@
 package com.mozcalti.gamingapp.service.correos;
 
-import com.mozcalti.gamingapp.exceptions.SendMailException;
-import com.mozcalti.gamingapp.exceptions.UtilsException;
 import com.mozcalti.gamingapp.model.Participantes;
 
 public interface SendMailInvitacionSevice {
