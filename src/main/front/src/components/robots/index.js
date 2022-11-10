5bdc0625270f1cfbe87c2cf5a63279341c13a935
Jose <joseangelesquivelislas@gmail.com>
2022-11-10 14:23:51 -0600

    Se soluciona error

--- src/main/front/src/components/robots/index.js
+++ src/main/front/src/components/robots/index.js
@@ -131,6 +131,7 @@ const Robots = () => {
     useEffect(() => {
         getParticipantePorCorreo(user.email)
         getRobots()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [idParticipante]);
 
     return (
