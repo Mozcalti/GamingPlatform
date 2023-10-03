4c3582d2f458de196b4de8ff5710890ad254cd59
jruiztahuilan <114958205+jruiztahuilan@users.noreply.github.com>
2023-10-03 09:34:24 -0600

    Ajuste para obtener etapa activa (#102)

--- src/main/java/com/mozcalti/gamingapp/service/equipo/impl/EtapaEquipoServiceImpl.java
+++ src/main/java/com/mozcalti/gamingapp/service/equipo/impl/EtapaEquipoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.mozcalti.gamingapp.service.equipo.impl;
 
 import com.mozcalti.gamingapp.commons.GenericServiceImpl;
+import com.mozcalti.gamingapp.model.Equipos;
 import com.mozcalti.gamingapp.model.EtapaEquipo;
+import com.mozcalti.gamingapp.repository.EquiposRepository;
 import com.mozcalti.gamingapp.repository.EtapaEquipoRepository;
 import com.mozcalti.gamingapp.repository.ParticipanteEquipoRepository;
 import com.mozcalti.gamingapp.service.equipo.EtapaEquipoService;
@@ -10,12 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.concurrent.atomic.AtomicReference;
+
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @Service
 public class EtapaEquipoServiceImpl extends GenericServiceImpl<EtapaEquipo, Integer> implements EtapaEquipoService {
 
     private EtapaEquipoRepository etapaEquipoRepository;
     private ParticipanteEquipoRepository participanteEquipoRepository;
+    private EquiposRepository equiposRepository;
     
     @Override
     public CrudRepository<EtapaEquipo, Integer> getDao() {
@@ -24,6 +30,20 @@ public class EtapaEquipoServiceImpl extends GenericServiceImpl<EtapaEquipo, Inte
 
     @Override
     public Integer getEtapaPorEquipo(Integer idParticipante) {
-        return etapaEquipoRepository.findByIdEquipo(participanteEquipoRepository.findByIdParticipante(idParticipante).getIdEquipo()).getIdEtapa();
+
+        AtomicReference<Integer> idEtapa = new AtomicReference<>();
+
+        participanteEquipoRepository.findAllByIdParticipante(idParticipante).forEach(
+                participanteEquipo -> {
+                    Equipos equipos = equiposRepository.findById(participanteEquipo.getIdEquipo())
+                            .orElseThrow(() -> new EntityNotFoundException("Equipo no encontrado"));
+
+                    if(equipos.isActivo()) {
+                        idEtapa.set(etapaEquipoRepository.findByIdEquipo(equipos.getIdEquipo()).getIdEtapa());
+                    }
+                }
+        );
+
+        return idEtapa.get();
     }
 }
